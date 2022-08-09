fe287cfeb5d5f31a1942bc1515a90c9a5e8aabc9
jurossimora <jurossimora@gmail.com>
2022-08-09 12:56:12 -0300

    Initial commit

---
+++ R/my_shannon.R
@@ -0,0 +1,16 @@
+#' My Shannon diversity function
+#'
+#' @param x A vector of numeric values representing the species abundance in a community
+#'
+#' @return A numeric vector of length 1 with the value of the Shannon diversity
+#' @export
+#'
+#' @examples
+#' com <- c(1, 2, 3)
+#' my_shannon(com)
+my_shannon <- function(x) {
+  x <- x[x > 0]
+  sum_all <- sum(x)
+  pi <- x/sum_all
+  -sum(pi*log(pi))
+}
